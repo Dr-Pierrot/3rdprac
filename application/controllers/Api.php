835e6502bc809d017d399088b697a9054c85aa07
Dr-Pierrot <Capulongako16@gmail.com>
2024-02-13 16:56:33 +0800

    123

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -30,7 +30,7 @@
        }
        
        public function insertpost(){
-         header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
+        //  header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
          header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS'); //method allowed
          header('Access-Control-Allow-Origin: *');
 
